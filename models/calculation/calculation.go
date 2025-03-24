5e8aa1eb0ff37e702f5a75f0e00070b67d1b7dab
devcrack <aurelio.hdz@yandex.com>
2025-03-24 12:23:58 -0600

    fix: Modified struct properties types

--- models/calculation/calculation.go
+++ models/calculation/calculation.go
@@ -1,35 +1,38 @@
 package calculation
 
-import "math/big"
+import (
+	"database/sql"
+	"math/big"
+)
 
 type Calculation struct {
-	EmployeeID         int64     `json:"employee_id"`
-	CountryID          int64     `json:"country_id"`
-	GroupID            int64     `json:"group_id"`
-	DivisionID         int64     `json:"division_id"`
-	DistrictID         int64     `json:"district_id"`
-	RegionID           int64     `json:"region_id"`
-	AreaID             int64     `json:"area_id"`
-	CompanyID          int64     `json:"company_id"`
-	EmployerRegistryID int64     `json:"employer_registry_id"`
-	BranchID           int64     `json:"branch_id"`
-	DepartmentID       int64     `json:"department_id"`
-	PositionId         int64     `json:"position_id"`
-	PayrollType        int64     `json:"payroll_type"`
-	Year               int       `json:"year"`
-	Month              int       `json:"month"`
-	CalendarID         int64     `json:"calendar_id"`
-	Period             int       `json:"period"`
-	ConceptType        int       `json:"concept_type"`
-	ConceptKey         string    `json:"concept_key"`
-	SatKey             string    `json:"sat_key"`
-	ConceptName        string    `json:"concept_name"`
-	Taxed              big.Float `json:"taxed"`
-	Exempt             big.Float `json:"exempt"`
-	Amount             big.Float `json:"amount"`
-	Days               int       `json:"days"`
-	IDUser             int64     `json:"id_user"`
-	Formula            string    `json:"formula"`
-	Interpreted        string    `json:"interpreted"`
-	PayrollID          int64     `json:"payroll_id"`
+	EmployeeID         sql.NullInt64  `json:"employee_id"`
+	CountryID          sql.NullInt64  `json:"country_id"`
+	GroupID            sql.NullInt64  `json:"group_id"`
+	DivisionID         sql.NullInt64  `json:"division_id"`
+	DistrictID         sql.NullInt64  `json:"district_id"`
+	RegionID           sql.NullInt64  `json:"region_id"`
+	AreaID             sql.NullInt64  `json:"area_id"`
+	CompanyID          sql.NullInt64  `json:"company_id"`
+	EmployerRegistryID sql.NullInt64  `json:"employer_registry_id"`
+	BranchID           sql.NullInt64  `json:"branch_id"`
+	DepartmentID       sql.NullInt64  `json:"department_id"`
+	PositionId         sql.NullInt64  `json:"position_id"`
+	PayrollType        sql.NullInt64  `json:"payroll_type"`
+	Year               sql.NullInt32  `json:"year"`
+	Month              sql.NullInt32  `json:"month"`
+	CalendarID         sql.NullInt64  `json:"calendar_id"`
+	Period             sql.NullInt32  `json:"period"`
+	ConceptType        sql.NullInt32  `json:"concept_type"`
+	ConceptKey         sql.NullString `json:"concept_key"`
+	SatKey             sql.NullString `json:"sat_key"`
+	ConceptName        sql.NullString `json:"concept_name"`
+	Taxed              *big.Float     `json:"taxed"`
+	Exempt             *big.Float     `json:"exempt"`
+	Amount             *big.Float     `json:"amount"`
+	Days               sql.NullInt32  `json:"days"`
+	IDUser             sql.NullInt64  `json:"id_user"`
+	Formula            sql.NullString `json:"formula"`
+	Interpreted        sql.NullString `json:"interpreted"`
+	PayrollID          sql.NullInt64  `json:"payroll_id"`
 }
